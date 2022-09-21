926a860e8ea052540450ecd2ec11b7f2850f37f7
Alec Hansen <alecdhansen@Alecs-MacBook-Air.local>
2022-09-21 15:01:11 -0400

    added repo fetch request

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-import reposContext from "./repos.js";
+// import reposContext from "./repos.js";
 import user from "./profile_info.js";
 import orgsContext from "./organizations.js";
-
+import apiKey from "./token.js";
 ("use strict");
 
 //--------//
+// const repos = {
+//   repos: json,
+// };
+function generateHTML(json) {
+  const repoSource = document.getElementById("repo-template").innerHTML;
+  const repoTemplate = Handlebars.compile(repoSource);
+  const repoHtml = repoTemplate({ repos: json });
+  console.log(json);
+  document
+    .querySelector(".repository-list")
+    .insertAdjacentHTML("afterbegin", repoHtml);
+}
 
-const repoSource = document.getElementById("repo-template").innerHTML;
-const repoTemplate = Handlebars.compile(repoSource);
-const repoHtml = repoTemplate(reposContext);
-
-document
-  .querySelector(".repository-list")
-  .insertAdjacentHTML("afterbegin", repoHtml);
+fetch(`https://api.github.com/users/alecdhansen/repos`, {
+  headers: {
+    Authorization: `token ${apiKey}`,
+  },
+})
+  .then((response) => response.json())
+  .then((json) => generateHTML(json));
 
 //--------//
 
 const userSource = document.getElementById("user-template").innerHTML;
 const userTemplate = Handlebars.compile(userSource);
 const userHtml = userTemplate(user);
-console.log(userHtml);
+// console.log(userHtml);
 
 document
   .querySelector(".user-section")
@@ -30,8 +42,10 @@ document
 const orgsSource = document.getElementById("orgs-template").innerHTML;
 const orgsTemplate = Handlebars.compile(orgsSource);
 const orgsHtml = orgsTemplate(orgsContext);
-console.log(orgsHtml);
+// console.log(orgsHtml);
 
 document
   .querySelector(".organization-avatars")
   .insertAdjacentHTML("afterbegin", orgsHtml);
+
+//--------//
